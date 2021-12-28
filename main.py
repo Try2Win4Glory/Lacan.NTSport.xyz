262669f161901cf3acffbd5c17ef41e2089da8f5
⚡Try2Win4Glory⚡ <65898600+Try2Win4Glory@users.noreply.github.com>
2021-12-28 12:42:55 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,19 @@
+from flask import Flask, request, render_template
+
+
+app = Flask(__name__)
+
+@app.route('/')
+def home(): 
+  return render_template('home.html')
+
+@app.route('/lacanhome')
+def lnshome():
+  return render_template('lacanhome.html')
+
+@app.route('/lacanhome2')
+def lnshome2():
+  return render_template('lacanhome2.html')
+  
+app.run(host='0.0.0.0')
+
